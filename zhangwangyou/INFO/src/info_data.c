f042dbcfede2285f18c9533d57e6442945a94d14
zwy <1227870330@qq.com>
2016-07-28 17:25:47 +0800

    info_data.c 1.增加INFO_data_FindNodeById函数，负责查找指定工号的节点 2.完成INFO_data_IsExist函数、INFO_data_IsEmpty函数、INFO_data_GetData函数、INFO_data_GetFirst函数、INFO_data_GetNext函数、INFO_data_Fini函数

--- zhangwangyou/INFO/src/info_data.c
+++ zhangwangyou/INFO/src/info_data.c
@@ -39,13 +39,65 @@ extern "C"{
 /* 信息数据结构 */
 typedef struct tagInfo_Data
 {
-    struct INFO_DATA_S *pstNext;   /* 数据组织相关[*]，指向下一个节点 */
-    INFO_CFG_S stCfg;              /* 配置数据 */
+    struct tagInfo_Data *pstNext;   /* 数据组织相关[*]，指向下一个节点 */
+    BOOL_T bIsEmpty;            /* 数据组织相关[*]，数据是否为空，TRUE表示空 */
+    INFO_CFG_S stCfg;           /* 配置数据 */
 }INFO_DATA_S;
 
 /* 数据链表头指针 */
 INFO_DATA_S *g_pstINFO_DATA_HEAD = NULL;
 
+
+/*****************************************************************************
+    Func Name: INFO_data_FindNodeById[*]
+ Date Created: 2016-07-28
+       Author: xxxx 00000
+  Description: 查找指定工号的节点是否存在
+        Input: IN UINT uiId         工号
+       Output:
+       Return: INFO_DATA_S *, != NULL     工号所在节点指针
+                              == NULL     未找到
+      Caution:
+------------------------------------------------------------------------------
+  Modification History
+  DATE        NAME             DESCRIPTION
+  --------------------------------------------------------------------------
+  YYYY-MM-DD
+
+*****************************************************************************/
+INFO_DATA_S *INFO_data_FindNodeById(IN UINT uiId)
+{
+    BOOL_T bRet = BOOL_FALSE;
+    INFO_DATA_S *pstNode = g_pstINFO_DATA_HEAD;
+
+    /* 遍历数据链表，查找工号 */
+    while ((NULL != pstNode) && (BOOL_TRUE != pstNode->bIsEmpty))
+    {
+        if (uiId == pstNode->stCfg.uiId)
+        {
+            bRet = BOOL_TRUE;
+            break;
+        }
+        else if (uiId < pstNode->stCfg.uiId)
+        {
+            bRet = BOOL_FALSE;
+            break;
+        }
+        else
+        {
+            pstNode = pstNode->pstNext;
+        }
+    }
+
+    if (BOOL_TRUE == bRet)
+    {
+        return pstNode;
+    }
+    else
+    {
+        return NULL;
+    }
+}
 
 /*****************************************************************************
     Func Name: INFO_data_IsExist[*]
@@ -66,7 +118,14 @@ INFO_DATA_S *g_pstINFO_DATA_HEAD = NULL;
 *****************************************************************************/
 BOOL_T INFO_data_IsExist(IN UINT uiId)
 {
-    return BOOL_FALSE;
+    if (NULL == INFO_data_FindNodeById(uiId))
+    {
+        return BOOL_FALSE;
+    }
+    else
+    {
+        return BOOL_TRUE;
+    }
 }
 
 /*****************************************************************************
@@ -88,7 +147,15 @@ BOOL_T INFO_data_IsExist(IN UINT uiId)
 *****************************************************************************/
 BOOL_T INFO_data_IsEmpty(VOID)
 {
-    return BOOL_TRUE;
+    /* 判断头节点的数据是否为空 */
+    if (BOOL_TRUE == g_pstINFO_DATA_HEAD->bIsEmpty)
+    {
+        return BOOL_TRUE;
+    }
+    else
+    {
+        return BOOL_FALSE;
+    }
 }
 
 /*****************************************************************************
@@ -110,7 +177,17 @@ BOOL_T INFO_data_IsEmpty(VOID)
 *****************************************************************************/
 ULONG INFO_data_GetData(IN UINT uiId, OUT INFO_CFG_S *pstCfg)
 {
-    return ERROR_FAILED;
+    INFO_DATA_S *pstNode = INFO_data_FindNodeById(uiId);
+
+    if (NULL != pstNode)
+    {
+        pstCfg = pstNode->stCfg;
+        return ERROR_SUCCESS;
+    }
+    else
+    {
+        return ERROR_FAILED;
+    }
 }
 
 /*****************************************************************************
@@ -132,7 +209,15 @@ ULONG INFO_data_GetData(IN UINT uiId, OUT INFO_CFG_S *pstCfg)
 *****************************************************************************/
 UINT INFO_data_GetFirst(VOID)
 {
-    return INFO_ID_INVALID;
+    if (BOOL_TRUE == INFO_data_IsEmpty())
+    {
+        return INFO_ID_INVALID;
+    }
+    else
+    {
+        /* 第一个有数据的工号在头节点处 */
+        return g_pstINFO_DATA_HEAD->stCfg.uiId;
+    }
 }
 
 /*****************************************************************************
@@ -154,7 +239,32 @@ UINT INFO_data_GetFirst(VOID)
 *****************************************************************************/
 UINT INFO_data_GetNext(IN UINT uiId)
 {
-    return INFO_ID_INVALID;
+    UINT uiRet = INFO_ID_INVALID;
+    INFO_DATA_S *pstNode = g_pstINFO_DATA_HEAD;
+
+    /* 遍历数据链表，查找工号 */
+    while ((NULL != pstNode) && (BOOL_TRUE != pstNode->bIsEmpty))
+    {
+        if (uiId < pstNode->stCfg.uiId)
+        {
+            /* 比uiId大的第一个工号的节点 */
+            uiRet = pstNode->stCfg.uiId;
+            break;
+        }
+        else
+        {
+            pstNode = pstNode->pstNext;
+        }
+    }
+
+    if (INFO_ID_INVALID != uiRet)
+    {
+        return uiRet;
+    }
+    else
+    {
+        return INFO_ID_INVALID;
+    }
 }
 
 /*****************************************************************************
@@ -166,7 +276,7 @@ UINT INFO_data_GetNext(IN UINT uiId)
        Output:
        Return: ULONG, ERROR_SUCCESS     处理成功
                       OTHER             处理失败
-      Caution: 目前始终成功
+      Caution:
 ------------------------------------------------------------------------------
   Modification History
   DATE        NAME             DESCRIPTION
@@ -180,9 +290,16 @@ ULONG INFO_data_Init(VOID)
 
     /* 创建数据链表头节点 */
     g_pstINFO_DATA_HEAD = (INFO_DATA_S *)malloc(sizeof(INFO_DATA_S));
+
     if (NULL == g_pstINFO_DATA_HEAD)
     {
         ulRet = ERROR_FAILED;
+    }
+    else
+    {
+        /* 初始化头节点 */
+        g_pstINFO_DATA_HEAD->pstNext = NULL;
+        g_pstINFO_DATA_HEAD->bIsEmpty = BOOL_TRUE;
     }
 
     if (ERROR_SUCCESS != ulRet)
@@ -213,6 +330,16 @@ ULONG INFO_data_Init(VOID)
 *****************************************************************************/
 VOID INFO_data_Fini(VOID)
 {
+    INFO_DATA_S *pstNode = NULL;
+
+    /* 遍历数据链表，释放数据链表的内存 */
+    while (NULL != g_pstINFO_DATA_HEAD)
+    {
+        pstNode = g_pstINFO_DATA_HEAD->pstNext;
+        free(g_pstINFO_DATA_HEAD);
+        g_pstINFO_DATA_HEAD = pstNode;
+    }
+
     return;
 }
 
